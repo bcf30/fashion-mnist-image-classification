9c5c7ba9d0cfb952d6342183d162e0dda646e181
bcf30 <gassyboi69@gmail.com>
2025-11-04 21:34:24 -0500

    g

--- train_with_model.py
+++ train_with_model.py
@@ -27,7 +27,7 @@ test_data = datasets.FashionMNIST(
 
 batch_size = 128
 
-train_dataloader = DataLoader(training_data, batch_size=batch_size,  pin_memory=True)
+train_dataloader = DataLoader(training_data, batch_size=batch_size, pin_memory=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size, pin_memory=True)
 
 for X, y in test_dataloader:
